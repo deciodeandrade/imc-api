0add54eaf8b732721285d961b84adcee7769d7fb
“Decio” <deciocavalcantedeandrade@gmail.com>
2022-01-10 05:52:09 -0300

    Autenticação JWT

--- app/controllers/imc_controller.rb
+++ app/controllers/imc_controller.rb
@@ -2,6 +2,8 @@ class ImcController < ApplicationController
   require './app/helpers/imc_helper'
   include ImcHelper
 
+  before_action :authorize_request
+
   def create
     imc = imc_calculate(params[:height].to_f, params[:weight].to_f) 
     @table_imc = classification_and_obesity_calculate(imc)
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
     scope "", defaults: {format: :json} do
         resources :imc, only: [:create, :show]
     end
+
+    resources :users, param: :_username
+    post '/auth/login', to: 'authentication#login'
+    get '/*a', to: 'application#not_found'
 end
